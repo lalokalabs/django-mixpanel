0b91433eae9cbbb873e5d00ce52308899b29b7f8
Kamal Mustafa <kamal@lalokalabs.co>
2022-11-28 09:30:04 +0000

    KAI518 do not initialize mixpanel if not post

--- django_mixpanel/middleware.py
+++ django_mixpanel/middleware.py
@@ -7,13 +7,15 @@ class DjangoMixpanelMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
-        # Code to be executed for each request before
-        # the view (and later middleware) are called.
+        if request.method != "POST":
+            response = self.get_response(request)
+            return response
+
         mixpanel = mixpanel_init(request)
         request.mixpanel = mixpanel
 
         response = self.get_response(request)
-        if response.status_code in (200, 201):
+        if response.status_code in (200, 201, 302):
             mixpanel_flush(request, response)
 
         # Code to be executed for each request/response after
